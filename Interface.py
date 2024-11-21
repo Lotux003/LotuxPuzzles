2e01d49fccdc5c5100cbdded5f96406d4a69ad82
Lotux <155517834+Lotux003@users.noreply.github.com>
2024-11-21 13:41:25 -0600

    Update Interface.py

--- Interface.py
+++ Interface.py
@@ -10,20 +10,23 @@ def spawn_program_and_die(program, exit_code=0):
     subprocess.Popen(program)
     sys.exit(exit_code)
 
-#spawn_program_and_die(['python', 'path/to/my/script.py'])
 
 def main():
     os.system('cls')
     Common.setTitle(f"Lotux Puzzles v{Common.THIS_VERSION}")
     Common.transition()
     Common.lotuxhometitle()
+    Common.LineSep()
     Common.lotuxhometree()
     choice = input("[#] Choice: ")
     if choice == "1" or "01":
         Common.transition()
-        spawn_program_and_die(['python', 'python Games/Maze.py'])
+        spawn_program_and_die(['python', 'Games/Maze.py'])
+        quit()
     elif choice == "2" or "02":
         Common.transition()
-        spawn_program_and_die(['python', 'python Games/TicTacToe.py'])
+        spawn_program_and_die(['python', 'Games/TicTacToe.py'])
+        quit()
+
     
 main()
